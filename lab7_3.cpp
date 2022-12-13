64798f28efeeca13a85756d446760cfce2169773
JayAleecha <aleecha.2546@gmail.com>
2022-12-13 16:44:15 +0700

    lab7_3

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
 
 using namespace std;
+int adiff(int a,int b){
+int c,d;
+if(a>b){
+  c=a-b;
+  d=c%360;
+  if(d>180){
+    return 360-d;
+  }else{
+    return d;
+  }
+}else{
+  c=b-a;
+   d=c%360;
+  if(d>180){
+    return 360-d;
+  }else{
+    return d;
+  }
+}
 
-
+}
 int main(){
   cout << adiff(180,270);
   cout << adiff(210,45);
